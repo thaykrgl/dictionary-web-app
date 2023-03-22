9857745ab23e9f4e47fead4ee7af3e0ea9c579d3
Taha AYKIROĞLU <taha.aykiroglu@karaca.com>
2023-03-22 21:29:12 +0300

    Updated script.js

--- script.js
+++ script.js
@@ -8,71 +8,81 @@ let sourceLink = document.getElementById("source-link");
 let nounSection = document.getElementById("noun-section");
 let verbSection = document.getElementById("verb-section");
 let meaningList = document.getElementById("meaning-list");
-
+let audioIcon = document.getElementById("audio-play");
 const sound = document.getElementById("sound");
+let searchElements = document.querySelectorAll(".word-search");
+
+
+// REMOVE ELEMENT FROM DOM
+function removeElementsByClass(className) {
+  const elements = document.getElementsByClassName(className);
+  while (elements.length > 0) {
+    elements[0].parentNode.removeChild(elements[0]);
+  }
+}
 
 
 // DARK MODE
 themeSelector.addEventListener("click", () => {
-    document.body.classList.toggle("dark-mode-bg");
+  document.body.classList.toggle("dark-mode-bg");
 })
 
+
 // GET DATA
-searchIcon.addEventListener("click", () => {
-    let search = searchInput.value;
-    let URL = `https://api.dictionaryapi.dev/api/v2/entries/en/${search}`;
-    let audioUrl;
-    nounSection.remove();
-    verbSection.remove();
-    // let sectionCont = document.getElementsByClassName("section-container");
-    // console.log(sectionCont);
-    // for (let i = 0; i < sectionCont.length; i++) {
-    //     console.log(sectionCont.length);
-    //     sectionCont[i].remove();
-    //     console.log(sectionCont[i]);
-    // }
-    // console.log(sectionCont);
+function getData(searchWord) {
 
-    fetch(URL)
-        .then(data => data.json())
-        .then(data => {
-            // console.log(data);
-            // let sectionCont = document.getElementsByClassName("section-container");
-            // console.log(sectionCont);
-            // for (let i = 0; i < sectionCont.length; i++) {
-            //     console.log(sectionCont.length);
-            //     console.log(sectionCont[i]);
-            //     sectionCont[i].remove();
-            // }
-            // console.log(sectionCont);
+  console.log(searchWord);
+  let URL = `https://api.dictionaryapi.dev/api/v2/entries/en/${searchWord}`;
+  let audioUrl;
+  nounSection.remove();
+  verbSection.remove();
+  removeElementsByClass("section-container");
 
-            // Word
-            word.innerHTML = data[0].word;
+  fetch(URL)
+    .then(data => data.json())
+    .then(data => {
 
-            // Phonetic
-            if (data[0].phonetic) {
-                phonetic.innerHTML = data[0].phonetic;
-            } else {
-                phonetic.innerHTML = "";
-            }
+      console.log(data);
 
-            // Audio
-            data[0].phonetics.forEach(element => {
-                if (element.audio != "") {
-                    audioUrl = element.audio;
-                }
-            })
-            sound.setAttribute("src", `${audioUrl}`);
+      // Word
+      word.innerHTML = data[0].word;
 
-            // Meanings
-            data[0].meanings.forEach(element => {
-                if (element.synonyms.length > 0) {
+      // Phonetic
+      if (data[0].phonetic) {
+        phonetic.innerHTML = data[0].phonetic;
+      } else {
+        phonetic.innerHTML = "";
+      }
 
-                    var node = document.querySelector(".first-row"),
-                        ele = document.createElement("div");
-                    ele.setAttribute("class", "section-container");
+      // Audio
+      let isAudio;
 
-                    ele.innerHTML = `<div class="section">
+      if (data[0].phonetics.length > 0) {
+        data[0].phonetics.forEach(element => {
+          if (element.audio != "") {
+            isAudio = true
+            audioUrl = element.audio;
+          } else {
+            audioIcon.style.display = "none";
+          }
+        })
+        if (isAudio) {
+          audioIcon.style.display = "block";
+        }
+        sound.setAttribute("src", `${audioUrl}`);
+      } else {
+        audioIcon.style.display = "none";
+      }
+
+      // Meanings
+      data[0].meanings.forEach(element => {
+        if (element.synonyms.length > 0) {
+
+          var node = document.querySelector(".first-row"),
+            ele = document.createElement("div");
+          ele.setAttribute("class", "section-container");
+
+          ele.innerHTML = `<div class="section">
                     <div class="seperator">
                       <div class="section-name">
                         <span>${element.partOfSpeech}</span>
@@ -84,31 +94,38 @@ searchIcon.addEventListener("click", () => {
                     </div>
                     <div class="section-content">
                       <ul id="meaning-list">
-                      
+
                       </ul>
                     </div>
-                    <div class="section-heading">
+                    <div class="section-heading" id="section-heading">
                       <h4>Synonyms</h4>
-                      <span>${element.synonyms[0]}</span>
                     </div>
                   </div>`;
-                    node.parentNode.insertBefore(ele, node.nextSibling);
+          node.parentNode.insertBefore(ele, node.nextSibling);
 
-                    element.definitions.forEach(item => {
-                        let meaningLi = document.createElement("li");
-                        document.getElementById("meaning-list").appendChild(meaningLi);
-                        let meaningSpan = document.createElement("span");
-                        meaningLi.appendChild(meaningSpan);
-                        meaningSpan.innerHTML = item.definition;
-                    });
+          element.definitions.forEach(item => {
+            let meaningLi = document.createElement("li");
+            document.getElementById("meaning-list").appendChild(meaningLi);
+            let meaningSpan = document.createElement("span");
+            meaningLi.appendChild(meaningSpan);
+            meaningSpan.innerHTML = item.definition;
+          });
 
-                } else {
+          element.synonyms.forEach(item => {
+            let synonymSpan = document.createElement("span");
+            document.getElementById("section-heading").appendChild(synonymSpan);
+            synonymSpan.innerHTML = item;
+            synonymSpan.setAttribute("onclick", `getData("${item}")`);
 
-                    var node = document.querySelector(".first-row"),
-                        ele = document.createElement("div");
-                    ele.setAttribute("class", "section-container");
+          });
 
-                    ele.innerHTML = `<div class="section">
+        } else {
+
+          var node = document.querySelector(".first-row"),
+            ele = document.createElement("div");
+          ele.setAttribute("class", "section-container");
+
+          ele.innerHTML = `<div class="section">
                         <div class="seperator">
                           <div class="section-name">
                             <span>${element.partOfSpeech}</span>
@@ -120,32 +137,37 @@ searchIcon.addEventListener("click", () => {
                         </div>
                         <div class="section-content">
                           <ul id="meaning-list">
-                            
+
                           </ul>
                         </div>
                       </div>`
 
-                    node.parentNode.insertBefore(ele, node.nextSibling);
+          node.parentNode.insertBefore(ele, node.nextSibling);
 
-                    element.definitions.forEach(item => {
-                        let meaningLi = document.createElement("li");
-                        document.getElementById("meaning-list").appendChild(meaningLi);
-                        let meaningSpan = document.createElement("span");
-                        meaningLi.appendChild(meaningSpan);
-                        meaningSpan.innerHTML = item.definition;
-                        let meaningExample = document.createElement("p");
-                        meaningLi.appendChild(meaningExample);
-                        meaningExample.innerHTML = `"${item.example}"`;
-                    })
-                }
-            })
+          element.definitions.forEach(item => {
+            let meaningLi = document.createElement("li");
+            document.getElementById("meaning-list").appendChild(meaningLi);
+            let meaningSpan = document.createElement("span");
+            meaningLi.appendChild(meaningSpan);
+            meaningSpan.innerHTML = item.definition;
+            let meaningExample = document.createElement("p");
+            meaningLi.appendChild(meaningExample);
+            let meaningHTML = item.example ? `"${item.example}"` : "";
+            meaningExample.innerHTML = meaningHTML;
+          })
+        }
+      })
 
-            // Source
-            source.innerHTML = data[0].sourceUrls[0];
-            sourceLink.setAttribute("href", data[0].sourceUrls[0])
-        })
+      // Source
+      source.innerHTML = data[0].sourceUrls[0];
+      sourceLink.setAttribute("href", data[0].sourceUrls[0])
+    })
+}
+
+searchIcon.addEventListener("click", function(){
+
 });
 
 function playSound() {
-    sound.play();
+  sound.play();
 }
